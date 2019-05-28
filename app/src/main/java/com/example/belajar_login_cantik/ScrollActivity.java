8f19c73f079a756dd5e1d63cf7a5a0060dc4924c
codepoin <codepoin.id@gmail.com>
2019-05-28 19:24:15 +0700

    display data from api

--- app/src/main/java/com/example/belajar_login_cantik/ScrollActivity.java
+++ app/src/main/java/com/example/belajar_login_cantik/ScrollActivity.java
@@ -1,21 +1,57 @@
 package com.example.belajar_login_cantik;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.belajar_login_cantik.Common.Common;
+import com.example.belajar_login_cantik.Model.OpenWeatherMap;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.squareup.picasso.Picasso;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-public class ScrollActivity extends AppCompatActivity {
+import io.paperdb.Paper;
+
+public class ScrollActivity extends AppCompatActivity implements LocationListener {
+
+    TextView txtCity, txtLastUpdate, txtDescription, txtHumidity, txtTime, txtCelsius;
+    ImageView imageView;
+
+    LocationManager locationManager;
+    String provider;
+    static double lat, lng;
+
+    OpenWeatherMap openWeatherMap = new OpenWeatherMap();
+
+    int MY_PERMISSION = 0;
+
     private CuacaAdapter adapter;
     private ArrayList<Kota> kotaArrayList;
     RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +81,43 @@ public class ScrollActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         recyclerView.setAdapter(adapter);
+
+        //Control
+//        txtCity = (TextView) findViewById(R.id.txtCity);
+//        txtLastUpdate = (TextView) findViewById(R.id.txtLastUpdate);
+        txtDescription = (TextView) findViewById(R.id.txtDescription);
+//        txtHumidity = (TextView) findViewById(R.id.txtHumidity);
+//        txtTime = (TextView) findViewById(R.id.txtTime);
+        txtCelsius = (TextView) findViewById(R.id.txtCelsius);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        String timeWidget = Common.getDateNow();
+        Paper.init(this);
+        Paper.book().write("temp",txtCelsius.getText().toString());
+        Paper.book().write("desc",txtDescription.getText().toString());
+        Paper.book().write("timeWidget",timeWidget);
+
+        //Get Coordinates
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+            ActivityCompat.requestPermissions(ScrollActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+
+            }, MY_PERMISSION);
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location == null)
+            Log.e("TAG","No Location");
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -57,5 +130,127 @@ public class ScrollActivity extends AppCompatActivity {
         kotaArrayList.add(new Kota("Fadly Yonk", "1214234560", "987654321"));
         kotaArrayList.add(new Kota("Ariyandi Nugraha", "1214230345", "987648765"));
         kotaArrayList.add(new Kota("Aham Siswana", "1214378098", "098758124"));
+    }
+
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ScrollActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+
+            }, MY_PERMISSION);
+        }
+        locationManager.removeUpdates(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ScrollActivity.this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_NETWORK_STATE,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+
+            }, MY_PERMISSION);
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    String langCode(){
+        return getResources().getString(R.string.lang);
+    }
+
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+
+
+        new ScrollActivity.GetWeather().execute(com.example.belajar_login_cantik.Common.Common.apiRequest(this.langCode(),String.valueOf(lat),String.valueOf(lng)));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    private class GetWeather extends AsyncTask<String,Void,String> {
+        ProgressDialog pd = new ProgressDialog(ScrollActivity.this);
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.setTitle("Please wait...");
+            pd.show();
+
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            String stream = null;
+            String urlString = params[0];
+
+            com.example.belajar_login_cantik.Helper.Helper http = new com.example.belajar_login_cantik.Helper.Helper();
+            stream = http.getHTTPData(urlString);
+            return stream;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s.contains("Error: Not found city")){
+                pd.dismiss();
+                return;
+            }
+            Gson gson = new Gson();
+            Type mType = new TypeToken<OpenWeatherMap>(){}.getType();
+            openWeatherMap = gson.fromJson(s,mType);
+            pd.dismiss();
+
+//            txtCity.setText(String.format("%s,%s",openWeatherMap.getName(),openWeatherMap.getSys().getCountry()));
+//            txtLastUpdate.setText(String.format("Last Updated: %s", com.codepoin.weatherassists.Common.Common.getDateNow()));
+            txtDescription.setText(String.format("%s",openWeatherMap.getWeather().get(0).getDescription()));
+//            txtHumidity.setText(String.format("%d%%",openWeatherMap.getMain().getHumidity()));
+//            txtTime.setText(String.format("%s/%s", com.codepoin.weatherassists.Common.Common.unixTimeStampToDateTime(openWeatherMap.getSys().getSunrise()), com.codepoin.weatherassists.Common.Common.unixTimeStampToDateTime(openWeatherMap.getSys().getSunset())));
+            txtCelsius.setText(String.format("%.2f °C",openWeatherMap.getMain().getTemp()));
+            Picasso.get()
+                    .load(com.example.belajar_login_cantik.Common.Common.getImage(openWeatherMap.getWeather().get(0).getIcon()))
+                    .into(imageView);
+
+        }
+
+    }
+
+    public void pengaturan(View view) {
+        Intent intent = new Intent(ScrollActivity.this, SettingActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void city(View view) {
+        Intent intent = new Intent(ScrollActivity.this, CityActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
